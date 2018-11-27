f20066c98d1bdc4b70514173ea3bfbe6fdc2202e
gabzon <gab.zambrano@gmail.com>
2018-11-27 11:22:13 +0100

    build added to git repository

---
+++ react/build/precache-manifest.69faf09b0bd9f68139deba1c11c1fb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae08a55e620d16b092af",
+    "url": "/static/js/main.ae08a55e.chunk.js"
+  },
+  {
+    "revision": "3a4bf4ffcbf981c4a69c",
+    "url": "/static/js/1.3a4bf4ff.chunk.js"
+  },
+  {
+    "revision": "ae08a55e620d16b092af",
+    "url": "/static/css/main.495f78ca.chunk.css"
+  },
+  {
+    "revision": "3a4bf4ffcbf981c4a69c",
+    "url": "/static/css/1.4dd81c4a.chunk.css"
+  },
+  {
+    "revision": "998d4c76e936dc32075f2f6022043465",
+    "url": "/index.html"
+  }
+];
